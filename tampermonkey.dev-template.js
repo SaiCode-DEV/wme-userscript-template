25ac31d16a07d8753777e5c673d8195c782ed448
SaiCode <saicode69@proton.me>
2025-02-10 14:24:47 +0100

    add live development server and dev userscript template

---
+++ tampermonkey.dev-template.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name WME Userscript Template [DEV]
+// @description A basic template for userscripts (Development Version)
+// @version 1.0.0-dev
+// @author Your name here
+// @match https://*.waze.com/*editor*
+// @match https://waze.com/*editor*
+// @match https://*.wazestg.com/*editor*
+// @connect localhost
+// @require http://localhost:{{PORT}}/wme-userscript.user.js
+// @grant GM_xmlhttpRequest
+// ==/UserScript==
