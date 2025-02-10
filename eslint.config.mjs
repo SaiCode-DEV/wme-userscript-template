0f7fea96b4e33638936cda970248c0a26008cf6d
SaiCode <saicode69@proton.me>
2025-02-10 13:09:43 +0100

    auto detect react version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,14 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
